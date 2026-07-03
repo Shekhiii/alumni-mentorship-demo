acd146d6d6cadead555bb9138bc80df8fe3167a2
Shekhar <shekhiii7393@gmail.com>
2026-07-03 23:50:04 +0530

    gmail prob

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 transporter.verify((err) => {
